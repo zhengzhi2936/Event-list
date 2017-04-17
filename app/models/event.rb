70f7e9f109c20090ad0b60b811ee1c9351bbcd96
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-04-17 15:16:35 +0800

    add select model to this app

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,9 @@ class Event < ApplicationRecord
  validates_uniqueness_of :friendly_id
  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
 
+ STATUS = ["draft", "public", "private"]
+ validates_inclusion_of :status, :in => STATUS
+
  # before_validation :generate_friendly_id, :on => :create
  def to_param
    self.friendly_id
