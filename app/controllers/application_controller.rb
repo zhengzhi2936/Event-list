0df940bb6c113adbec4802bb1138cb68c0d6b046
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-04-17 09:24:57 +0800

    add mutiple language to this app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+     I18n.locale = session[:locale] || I18n.default_locale
+  end
   protect_from_forgery with: :exception
 end
