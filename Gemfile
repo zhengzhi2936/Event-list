a2274e530536d364bd58c43fab502ed5b7a72d74
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-05-05 09:24:26 +0800

    add event ticket to this app

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'jbuilder', '~> 2.5'
 gem "rails-i18n"
 gem "devise-i18n"
 gem "select2-rails"
+gem "nested_form_fields"
 group :development, :test do
   gem 'rspec-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,8 @@ class Event < ApplicationRecord
 
  STATUS = ["draft", "public", "private"]
  validates_inclusion_of :status, :in => STATUS
-
+ has_many :tickets, :dependent => :destroy
+ accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
  # before_validation :generate_friendly_id, :on => :create
  def to_param
    self.friendly_id
