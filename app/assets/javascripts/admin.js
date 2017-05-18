e1ba778f6591f9726579f09e1abb647a5182558a
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-05-18 17:41:21 +0800

    delete torbulinks

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
