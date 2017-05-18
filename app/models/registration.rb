fedb0b19afbffce38ebe563ba882c0e114e3a26e
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-05-18 15:31:40 +0800

    add seperate step to this app

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -9,12 +9,22 @@ class Registration < ApplicationRecord
   belongs_to :user, :optional => true
 
   before_validation :generate_uuid, :on => :create
-
+  attr_accessor :current_step
+  validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
+  validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
   def to_param
     self.uuid
   end
 
   protected
+  
+  def should_validate_basic_data?
+    current_step == 2  # 只有做到第二步需要验证
+  end
+
+  def should_validate_all_data?
+    current_step == 3 || status == "confirmed"  # 做到第三步，或最后状态是 confirmed 时需要验证
+  end
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
